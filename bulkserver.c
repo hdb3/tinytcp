16b6364660982b83da90b5b8e368ed72338175ac
Nic Hart <nic@nic-ThinkPad-X61s>
2016-04-17 13:03:25 +0100

    parse multipliers on the request size...

--- bulkserver.c
+++ bulkserver.c
@@ -23,21 +23,39 @@ void Die(char *mess) { perror(mess); exit(1); }
 
 void HandleClient(int sock) {
   char buffer[BUFFSIZE];
+  char *tailptr;
   int received = -1;
   long long int respsize,sendsize;
   char* blockptr = malloc(BLOCKSIZE);
   memset(blockptr,0xaa,BLOCKSIZE);
 
   /* Receive message */
-  if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
+  if ((received = recv(sock, buffer, BUFFSIZE-1, 0)) < 0) {
     Die("Failed to receive initial bytes from client");
   }
   /* get the client request - for now, just expect a number which is the size of the
      expected response message */
-  respsize = strtoll(buffer, NULL,10);
+  buffer[received] = 0; /* parsing needs to know where the input string ends.... */
+  respsize = strtoll(buffer, &tailptr,10);
+  fprintf(stderr, "client request (\"%s\")",buffer);
   if (errno) {
-      fprintf(stderr, "client request (\"%s\")",buffer);
-      Die("Failed to parse client request");
+      Die("\nFailed to parse client request");
+    } else {
+      switch ( *tailptr ) {
+        case 'k' :
+        case 'K' :
+          respsize <<= 10;
+          break;
+        case 'm' :
+        case 'M' :
+          respsize <<= 20;
+          break;
+        case 'g' :
+        case 'G' :
+          respsize <<= 30;
+          break;
+      };
+      fprintf(stderr, ", parsed request as %lld bytes\n",respsize);
     }
   /* Send bytes and check for more incoming data in loop */
 #ifdef ZEROCOPY
